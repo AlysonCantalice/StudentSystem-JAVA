09971e75f03df47776cdf4316559a650f5ccd083
Alyson Cantalice <alysoncantalice@gmail.com>
2023-08-25 00:59:15 -0300

    Added features

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,9 +15,9 @@ public class Program {
 
 		List<Student> students = new ArrayList<>();
 		boolean exit = false;
-		System.out.println("Welcome to the Student Registration System!");
 
 		while (true) {
+			System.out.println("Welcome to the Student Registration System!");
 			System.out.println();
 			System.out.println("(1) Register a new student");
 			System.out.println("(2) Find a student");
@@ -43,11 +43,11 @@ public class Program {
 					System.out.print("Age: ");
 					int age = sc.nextInt();
 					System.out.print("Registration number: ");
-					int registration = sc.nextInt();
+					int registrationNumber = sc.nextInt();
 					System.out.print("Phone Number: ");
 					int phoneNumber = sc.nextInt();
 
-					students.add(new Student(name, age, registration, phoneNumber));
+					students.add(new Student(name, age, registrationNumber, phoneNumber));
 
 					System.out.println("Student registered successfully!");
 					System.out.print("Do you wish to register another student? (Y/N): ");
@@ -61,30 +61,144 @@ public class Program {
 						break;
 					}
 				}
+				break;
 			}
 			// Find student
 			case 2: {
-				// 
+				System.out.println();
+				if (students.size() == 0) {
+					System.out.println("There are no students to find.");
+					pressEnter();
+				} else {
+					System.out.print("Enter the registration number of the student: ");
+					int registrationNumber = sc.nextInt();
+					boolean found = false;
+					for (Student student : students) {
+						if (student.getRegistration() == registrationNumber) {
+							System.out.println(student);
+							found = true;
+							pressEnter();
+							break;
+						}
+					}
+					if (found == false) {
+						System.out.println("Student not found.");
+						pressEnter();
+					}
+				}
 				break;
 			}
 			// Show all students
 			case 3: {
 				System.out.println();
-				int i = 1;
-				for (Student student : students) {
-					System.out.println("Student " + i);
-					System.out.println(student);
-					i++;
+				if (students.size() == 0) {
+					System.out.println("There are no students to show.");
+					pressEnter();
+				} else {
+					int i = 1;
+					for (Student student : students) {
+						System.out.println("Student " + i);
+						System.out.println(student);
+						i++;
+					}
+					pressEnter();
 				}
-				pressEnter();
 				break;
 			}
 			// update student
 			case 4: {
+				System.out.println();
+				if (students.size() == 0) {
+					System.out.println("There are no students to update.");
+					pressEnter();
+				} else {
+					System.out.print("Enter the registration number of the student: ");
+					int registrationNumber = sc.nextInt();
+					Student foundStudent = null;
+					for (Student student : students) {
+						if (student.getRegistration() == registrationNumber) {
+							foundStudent = student;
+							break;
+						}
+					}
+					if (foundStudent == null) {
+						System.out.println("Student not found.");
+						pressEnter();
+					} else {
+						System.out.println("(1) Name: " + foundStudent.getName());
+						System.out.println("(2) Age: " + foundStudent.getAge());
+						System.out.println("(3) Registration N.: " + foundStudent.getRegistration());
+						System.out.println("(4) Phone Number: " + foundStudent.getPhoneNumber());
+						System.out.println();
+						System.out.println("Which field will be updated? ");
+						int answer = sc.nextInt();
+
+						switch (answer) {
+						case 1: {
+							System.out.print("New name: ");
+							sc.nextLine();
+							String name = sc.nextLine();
+							foundStudent.setName(name);
+							System.out.println("Name updated.");
+							pressEnter();
+							break;
+						}
+						case 2: {
+							System.out.print("New age: ");
+							sc.nextLine();
+							int age = sc.nextInt();
+							foundStudent.setAge(age);
+							System.out.println("Age updated.");
+							pressEnter();
+							break;
+						}
+						case 3: {
+							System.out.print("New registration number: ");
+							sc.nextLine();
+							registrationNumber = sc.nextInt();
+							foundStudent.setRegistration(registrationNumber);
+							System.out.println("Registration number updated.");
+							pressEnter();
+							break;
+						}
+						case 4: {
+							System.out.print("New phone number: ");
+							sc.nextLine();
+							int phoneNumber = sc.nextInt();
+							foundStudent.setPhoneNumber(phoneNumber);
+							System.out.println("Phone Number updated.");
+							pressEnter();
+							break;
+						}
+						}
+					}
+				}
 				break;
 			}
 			// Remove student
 			case 5: {
+				System.out.println();
+				if (students.size() == 0) {
+					System.out.println("There are no students to remove.");
+					pressEnter();
+				} else {
+					System.out.print("Enter the registration number of the student: ");
+					int registrationNumber = sc.nextInt();
+					boolean found = false;
+					for (Student student : students) {
+						if (student.getRegistration() == registrationNumber) {
+							students.remove(student);
+							found = true;
+							System.out.println("Student removed.");
+							pressEnter();
+							break;
+						}
+					}
+					if (found == false) {
+						System.out.println("Student not found.");
+						pressEnter();
+					}
+				}
 				break;
 			}
 			// Import CSV
@@ -106,6 +220,7 @@ public class Program {
 			// Invalid option
 			default: {
 				System.out.println("Invalid option! Please, choose a valid option.");
+				pressEnter();
 				break;
 			}
 			}
@@ -113,6 +228,7 @@ public class Program {
 			if (exit == true) {
 				break;
 			}
+			clearScreen();
 		}
 
 		sc.close();
@@ -125,5 +241,10 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		sc.nextLine();
 		// Won't close the scanner otherwise we will have problems in the main program
+	}
+
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
 	}
 }
