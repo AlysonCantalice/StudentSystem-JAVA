9665ff0211bd797c7537ab7eab6d6d4034e71bad
Alyson Cantalice <alysoncantalice@gmail.com>
2023-08-25 20:43:57 -0300

    Input validation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -29,200 +30,210 @@ public class Program {
 			System.out.println();
 			System.out.print("Please choose your desired option: ");
 
-			int choice = sc.nextInt();
-			switch (choice) {
-			// Add new student
-			case 1: {
-				while (true) {
-					System.out.println();
-					System.out.println("Registering a new Student");
-					sc.nextLine();
-					System.out.print("Name: ");
-					String name = sc.nextLine();
-					System.out.print("Age: ");
-					int age = sc.nextInt();
-					System.out.print("Registration number: ");
-					int registrationNumber = sc.nextInt();
-					while (studentRegistration.checkRegistration(registrationNumber) == true) {
-						System.out.print("Registration number already taken. Please choose another: ");
-						registrationNumber = sc.nextInt();
-					}
-					System.out.print("Phone Number: ");
-					int phoneNumber = sc.nextInt();
-
-					studentRegistration.addStudent(name, age, registrationNumber, phoneNumber);
-
-					System.out.println("Student registered successfully!");
-					System.out.print("Do you wish to register another student? (Y/N): ");
-					sc.nextLine();
-					char answer = sc.next().toUpperCase().charAt(0);
-					while (answer != 'Y' && answer != 'N') {
-						System.out.print("Invalid! Choose a valid answer (Y/N): ");
-						answer = sc.next().toUpperCase().charAt(0);
-					}
-					if (answer == 'N') {
-						break;
-					}
-				}
-				break;
-			}
-			// Find student
-			case 2: {
-				System.out.println();
-				if (studentRegistration.isThereStudents() == false) {
-					System.out.println("There are no students to find.");
-					pressEnter();
-				} else {
-					System.out.print("Enter the registration number of the student: ");
-					int registrationNumber = sc.nextInt();
-					Student foundStudent = studentRegistration.findStudent(registrationNumber);
-					if (foundStudent != null) {
-						System.out.println(foundStudent);
-						pressEnter();
-					} else {
-						System.out.println("Student not found.");
-						pressEnter();
-					}
-				}
-				break;
-			}
-			// Show all students
-			case 3: {
-				System.out.println();
-				if (studentRegistration.isThereStudents() == false) {
-					System.out.println("There are no students to show.");
-					pressEnter();
-				} else {
-					studentRegistration.showStudents();
-					pressEnter();
-				}
-				break;
-			}
-			// update student
-			case 4: {
-				System.out.println();
-				if (studentRegistration.isThereStudents() == false) {
-					System.out.println("There are no students to update.");
-					pressEnter();
-				} else {
-					System.out.print("Enter the registration number of the student: ");
-					int registrationNumber = sc.nextInt();
-					Student foundStudent = studentRegistration.findStudent(registrationNumber);
-					if (foundStudent == null) {
-						System.out.println("Student not found.");
-						pressEnter();
-					} else {
-						System.out.println("(1) Name: " + foundStudent.getName());
-						System.out.println("(2) Age: " + foundStudent.getAge());
-						System.out.println("(3) Registration N.: " + foundStudent.getRegistration());
-						System.out.println("(4) Phone Number: " + foundStudent.getPhoneNumber());
+			try {
+				int choice = sc.nextInt();
+				switch (choice) {
+				// Add new student
+				case 1: {
+					while (true) {
 						System.out.println();
-						System.out.println("Which field will be updated? ");
-						int answer = sc.nextInt();
-
-						switch (answer) {
-						case 1: {
-							System.out.print("New name: ");
-							sc.nextLine();
-							String name = sc.nextLine();
-							foundStudent.setName(name);
-							System.out.println("Name updated.");
-							pressEnter();
-							break;
-						}
-						case 2: {
-							System.out.print("New age: ");
-							sc.nextLine();
-							int age = sc.nextInt();
-							foundStudent.setAge(age);
-							System.out.println("Age updated.");
-							pressEnter();
-							break;
-						}
-						case 3: {
-							System.out.print("New registration number: ");
-							sc.nextLine();
+						System.out.println("Registering a new Student");
+						sc.nextLine();
+						System.out.print("Name: ");
+						String name = sc.nextLine();
+						System.out.print("Age: ");
+						int age = sc.nextInt();
+						System.out.print("Registration number: ");
+						int registrationNumber = sc.nextInt();
+						while (studentRegistration.checkRegistration(registrationNumber) == true) {
+							System.out.print("Registration number already taken. Please choose another: ");
 							registrationNumber = sc.nextInt();
-							while (studentRegistration.checkRegistration(registrationNumber) == true) {
-								System.out.print("Registration number already taken. Please choose another: ");
-								registrationNumber = sc.nextInt();
-							}
-							foundStudent.setRegistration(registrationNumber);
-							System.out.println("Registration number updated.");
-							pressEnter();
+						}
+						System.out.print("Phone Number: ");
+						int phoneNumber = sc.nextInt();
+
+						studentRegistration.addStudent(name, age, registrationNumber, phoneNumber);
+
+						System.out.println("Student registered successfully!");
+						System.out.print("Do you wish to register another student? (Y/N): ");
+						sc.nextLine();
+						char answer = sc.next().toUpperCase().charAt(0);
+						while (answer != 'Y' && answer != 'N') {
+							System.out.print("Invalid! Choose a valid answer (Y/N): ");
+							answer = sc.next().toUpperCase().charAt(0);
+						}
+						if (answer == 'N') {
 							break;
 						}
-						case 4: {
-							System.out.print("New phone number: ");
-							sc.nextLine();
-							int phoneNumber = sc.nextInt();
-							foundStudent.setPhoneNumber(phoneNumber);
-							System.out.println("Phone Number updated.");
+
+					}
+					break;
+				}
+				// Find student
+				case 2: {
+					System.out.println();
+					if (studentRegistration.isThereStudents() == false) {
+						System.out.println("There are no students to find.");
+						pressEnter();
+					} else {
+						System.out.print("Enter the registration number of the student: ");
+						int registrationNumber = sc.nextInt();
+						Student foundStudent = studentRegistration.findStudent(registrationNumber);
+						if (foundStudent != null) {
+							System.out.println(foundStudent);
 							pressEnter();
-							break;
-						}
+						} else {
+							System.out.println("Student not found.");
+							pressEnter();
 						}
 					}
+					break;
 				}
-				break;
-			}
-			// Remove student
-			case 5: {
-				System.out.println();
-				if (studentRegistration.isThereStudents() == false) {
-					System.out.println("There are no students to remove.");
-					pressEnter();
-				} else {
-					System.out.print("Enter the registration number of the student: ");
-					int registrationNumber = sc.nextInt();
-					studentRegistration.removeStudent(registrationNumber);
-					pressEnter();
+				// Show all students
+				case 3: {
+					System.out.println();
+					if (studentRegistration.isThereStudents() == false) {
+						System.out.println("There are no students to show.");
+						pressEnter();
+					} else {
+						studentRegistration.showStudents();
+						pressEnter();
+					}
+					break;
 				}
-				break;
-			}
-			// Import CSV
-			case 6: {
-				System.out.println("Import from CSV");
-				System.out.print("Directory path where the CSV is. ex(c:\\\\temp\\\\StudentData): ");
+				// update student
+				case 4: {
+					System.out.println();
+					if (studentRegistration.isThereStudents() == false) {
+						System.out.println("There are no students to update.");
+						pressEnter();
+					} else {
+						System.out.print("Enter the registration number of the student: ");
+						int registrationNumber = sc.nextInt();
+						Student foundStudent = studentRegistration.findStudent(registrationNumber);
+						if (foundStudent == null) {
+							System.out.println("Student not found.");
+							pressEnter();
+						} else {
+							System.out.println("(1) Name: " + foundStudent.getName());
+							System.out.println("(2) Age: " + foundStudent.getAge());
+							System.out.println("(3) Registration N.: " + foundStudent.getRegistration());
+							System.out.println("(4) Phone Number: " + foundStudent.getPhoneNumber());
+							System.out.println();
+							System.out.println("Which field will be updated? ");
+							int answer = sc.nextInt();
+
+							switch (answer) {
+							case 1: {
+								System.out.print("New name: ");
+								sc.nextLine();
+								String name = sc.nextLine();
+								foundStudent.setName(name);
+								System.out.println("Name updated.");
+								pressEnter();
+								break;
+							}
+							case 2: {
+								System.out.print("New age: ");
+								sc.nextLine();
+								int age = sc.nextInt();
+								foundStudent.setAge(age);
+								System.out.println("Age updated.");
+								pressEnter();
+								break;
+							}
+							case 3: {
+								System.out.print("New registration number: ");
+								sc.nextLine();
+								registrationNumber = sc.nextInt();
+								while (studentRegistration.checkRegistration(registrationNumber) == true) {
+									System.out.print("Registration number already taken. Please choose another: ");
+									registrationNumber = sc.nextInt();
+								}
+								foundStudent.setRegistration(registrationNumber);
+								System.out.println("Registration number updated.");
+								pressEnter();
+								break;
+							}
+							case 4: {
+								System.out.print("New phone number: ");
+								sc.nextLine();
+								int phoneNumber = sc.nextInt();
+								foundStudent.setPhoneNumber(phoneNumber);
+								System.out.println("Phone Number updated.");
+								pressEnter();
+								break;
+							}
+							}
+						}
+					}
+					break;
+				}
+				// Remove student
+				case 5: {
+					System.out.println();
+					if (studentRegistration.isThereStudents() == false) {
+						System.out.println("There are no students to remove.");
+						pressEnter();
+					} else {
+						System.out.print("Enter the registration number of the student: ");
+						int registrationNumber = sc.nextInt();
+						studentRegistration.removeStudent(registrationNumber);
+						pressEnter();
+					}
+					break;
+				}
+				// Import CSV
+				case 6: {
+					System.out.println("Import from CSV");
+					System.out.print("Directory path where the CSV is. ex(c:\\\\temp\\\\StudentData): ");
+					sc.nextLine();
+					String strPath = sc.nextLine();
+
+					studentRegistration.importCSV(strPath);
+					pressEnter();
+					break;
+				}
+				// Export CSV
+				case 7: {
+					System.out.println("Export to CSV");
+					System.out.print("Directory path ex(c:\\\\temp\\\\StudentData): ");
+					sc.nextLine();
+					String strPath = sc.nextLine();
+
+					studentRegistration.exportCSV(strPath);
+					pressEnter();
+					break;
+				}
+				// Exit
+				case 8: {
+					System.out.println();
+					System.out.println("Have a nice day!");
+					exit = true;
+					break;
+				}
+				// Invalid option
+				default: {
+					System.out.println("Invalid option! Please, choose a valid option.");
+					pressEnter();
+					break;
+				}
+				}
+
+				if (exit == true) {
+					break;
+				}
+
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input, please enter integer numbers.");
+				pressEnter();
 				sc.nextLine();
-				String strPath = sc.nextLine();
-
-				studentRegistration.importCSV(strPath);
+			} catch (RuntimeException e) {
+				System.out.println("Unexpected error.");
 				pressEnter();
-				break;
-			}
-			// Export CSV
-			case 7: {
-				System.out.println("Export to CSV");
-				System.out.print("Directory path ex(c:\\\\temp\\\\StudentData): ");
 				sc.nextLine();
-				String strPath = sc.nextLine();
-
-				studentRegistration.exportCSV(strPath);
-				pressEnter();
-				break;
 			}
-			// Exit
-			case 8: {
-				System.out.println();
-				System.out.println("Have a nice day!");
-				exit = true;
-				break;
-			}
-			// Invalid option
-			default: {
-				System.out.println("Invalid option! Please, choose a valid option.");
-				pressEnter();
-				break;
-			}
-			}
-
-			if (exit == true) {
-				break;
-			}
-
 			clearScreen();
-
 		}
 
 		sc.close();
